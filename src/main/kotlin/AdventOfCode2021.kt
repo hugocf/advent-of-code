d4bb5973ef9d8ab770be57836125c055fd67d81c
Hugo Ferreira <hugocf@users.noreply.github.com>
2021-12-01 23:11:20 +0000

    2021: Puzzle 1 part 2

--- src/main/kotlin/AdventOfCode2021.kt
+++ src/main/kotlin/AdventOfCode2021.kt
@@ -1,3 +1,5 @@
 object Puzzle01 {
-    fun countIncreasingDepths(depths: List<Int>) = depths.zipWithNext() { a, b -> if (a < b) 1 else 0 }.sum()
+    fun countIncreasingDepths(depths: List<Int>) = depths.zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
+
+    fun countIncreasingDepthsBy(size: Int, depths: List<Int>) = countIncreasingDepths(depths.windowed(size) { it.sum() })
 }
--- src/test/kotlin/AdventOfCode2021KtTest.kt
+++ src/test/kotlin/AdventOfCode2021KtTest.kt
@@ -1,5 +1,7 @@
 import Puzzle01.countIncreasingDepths
-import org.junit.jupiter.api.Assertions.*
+import Puzzle01.countIncreasingDepthsBy
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import java.io.File
@@ -8,18 +10,35 @@ internal class AdventOfCode2021KtTest {
 
     @Nested
     inner class Puzzle01 {
+        val exampleList = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+
         @Test
         fun `count increasing depths example`() {
-            assertEquals(7, countIncreasingDepths(listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)))
+            assertEquals(7, countIncreasingDepths(exampleList))
         }
 
         @Test
         fun `count increasing depths answer`() {
-            val sourceName = "puzzle01.input.txt"
-            val sourceUri = this.javaClass.getResource(sourceName)?.toURI() ?: fail("Cannot file resource: $sourceName")
-            val depths = File(sourceUri).readLines().map { it.toInt() }
-
+            val depths = Context.readLinesFromResource("puzzle01.input.txt").map { it.toInt() }
             assertEquals(1624, countIncreasingDepths(depths))
+        }
+
+        @Test
+        fun `count window depths example`() {
+            assertEquals(5, countIncreasingDepthsBy(3, exampleList))
+        }
+
+        @Test
+        fun `count window depths answer`() {
+            val depths = Context.readLinesFromResource("puzzle01.input.txt").map { it.toInt() }
+            assertEquals(1653, countIncreasingDepthsBy(3, depths))
+        }
+    }
+
+    private object Context {
+        fun readLinesFromResource(fileName: String): List<String> {
+            val uri = this.javaClass.getResource(fileName)?.toURI() ?: fail("Cannot find resource: $fileName")
+            return File(uri).readLines()
         }
     }
 }
