14c0895413b1bdf8c54974210325a27ae0a447ef
Hugo Ferreira <hugocf@users.noreply.github.com>
2021-12-02 11:33:36 +0000

    2021: Puzzle 2 part 1

--- src/main/kotlin/AdventOfCode2021.kt
+++ src/main/kotlin/AdventOfCode2021.kt
@@ -1,5 +1,28 @@
 object Puzzle01 {
-    fun countIncreasingDepths(depths: List<Int>) = depths.zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
+    fun countIncreasingDepths(depths: List<Int>): Int =
+        depths.zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
 
-    fun countIncreasingDepthsBy(size: Int, depths: List<Int>) = countIncreasingDepths(depths.windowed(size) { it.sum() })
+    fun countIncreasingDepthsBy(size: Int, depths: List<Int>): Int =
+        countIncreasingDepths(depths.windowed(size) { it.sum() })
+}
+
+data class Position(val horizontal: Int, val depth: Int)
+
+object Puzzle02 {
+    fun moveSubmarine(start: Position, commands: List<String>): Position {
+        val delimiter = " "
+        val grouped = commands
+            .map { Pair(it.split(delimiter).first(), it.split(delimiter).last()) }
+            .groupBy { it.first }
+            .mapValues { entry -> entry.value.map { it.second.toInt() } }
+
+        val moveForward = grouped["forward"]?.sum() ?: 0
+        val moveUp = grouped["up"]?.sum() ?: 0
+        val moveDown = grouped["down"]?.sum() ?: 0
+
+        return Position(
+            start.horizontal + moveForward,
+            start.depth + moveDown - moveUp,
+        )
+    }
 }
--- src/test/kotlin/AdventOfCode2021KtTest.kt
+++ src/test/kotlin/AdventOfCode2021KtTest.kt
@@ -1,5 +1,6 @@
 import Puzzle01.countIncreasingDepths
 import Puzzle01.countIncreasingDepthsBy
+import Puzzle02.moveSubmarine
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Nested
@@ -10,11 +11,11 @@ internal class AdventOfCode2021KtTest {
 
     @Nested
     inner class Puzzle01 {
-        val exampleList = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
+        val exampleDepths = listOf(199, 200, 208, 210, 200, 207, 240, 269, 260, 263)
 
         @Test
         fun `count increasing depths example`() {
-            assertEquals(7, countIncreasingDepths(exampleList))
+            assertEquals(7, countIncreasingDepths(exampleDepths))
         }
 
         @Test
@@ -25,13 +26,45 @@ internal class AdventOfCode2021KtTest {
 
         @Test
         fun `count window depths example`() {
-            assertEquals(5, countIncreasingDepthsBy(3, exampleList))
+            assertEquals(5, countIncreasingDepthsBy(3, exampleDepths))
         }
 
         @Test
         fun `count window depths answer`() {
             val depths = Context.readLinesFromResource("puzzle01.input.txt").map { it.toInt() }
             assertEquals(1653, countIncreasingDepthsBy(3, depths))
+        }
+    }
+
+    @Nested
+    inner class Puzzle02 {
+        val exampleCommands = listOf("forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2")
+
+        @Test
+        fun `move submarine example`() {
+            val result = moveSubmarine(start= Position(0, 0), exampleCommands)
+
+            assertEquals(150, result.horizontal * result.depth)
+        }
+
+        @Test
+        fun `move submarine answer`() {
+            val commands = Context.readLinesFromResource("puzzle02.input.txt")
+
+            val result = moveSubmarine(start= Position(0, 0),  commands)
+
+            assertEquals(1746616, result.horizontal * result.depth)
+        }
+
+        @Test
+        fun `count window depths example`() {
+            // assertEquals(5, countIncreasingDepthsBy(3, exampleCommands))
+        }
+
+        @Test
+        fun `count window depths answer`() {
+            //val depths = Context.readLinesFromResource("puzzle01.input.txt").map { it.toInt() }
+            //assertEquals(1653, countIncreasingDepthsBy(3, depths))
         }
     }
 
