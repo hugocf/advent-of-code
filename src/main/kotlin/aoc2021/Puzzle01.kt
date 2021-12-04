b739eb5bcfca50889eaae61e8f12e9941823bf71
Hugo Ferreira <hugocf@users.noreply.github.com>
2021-12-04 12:22:21 +0000

    2021: Puzzle 1 part 1 alternative

--- src/main/kotlin/aoc2021/Puzzle01.kt
+++ src/main/kotlin/aoc2021/Puzzle01.kt
@@ -2,7 +2,7 @@ package aoc2021
 
 object Puzzle01 {
     fun countIncreasingDepths(depths: List<Int>): Int =
-        depths.zipWithNext { a, b -> if (a < b) 1 else 0 }.sum()
+        depths.zipWithNext().count { it.first < it.second }
 
     fun countIncreasingDepthsBy(size: Int, depths: List<Int>): Int =
         countIncreasingDepths(depths.windowed(size) { it.sum() })
