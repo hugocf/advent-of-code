b76aa7b90c082817efbb4b9340bedc3323bc6166
Hugo Ferreira <hugocf@users.noreply.github.com>
2021-12-10 11:47:27 +0000

    2021: Puzzle 6 part 2

--- src/main/kotlin/aoc2021/Puzzle06.kt
+++ src/main/kotlin/aoc2021/Puzzle06.kt
@@ -1,13 +1,30 @@
 package aoc2021
 
 object Puzzle06 {
-    fun countFishAfterDays(days: Int, input: String): Int {
-        val initialState = input.split(",").map(String::toInt)
-        val fishAfterDays = (1..days).fold(initialState) { acc, _ ->
+    fun naiveCountFishAfterDays(days: Int, input: String): Int {
+        val start = parseInitialState(input)
+
+        val fishAfterDays = (1..days).fold(start) { acc, _ ->
             val nextDay = acc.map { if (it - 1 < 0) 6 else it - 1 }
             val newBorn = acc.filter { it == 0 }.map { 8 }
             nextDay + newBorn
         }
+
         return fishAfterDays.size
     }
+
+    fun optimisedCountFishAfterDays(days: Int, input: String): Long {
+        val initialState = parseInitialState(input)
+        val fishDaysLeft = MutableList<Long>(9) { idx -> initialState.count { it == idx }.toLong() }
+
+        repeat(days) {
+            val toRespawn = fishDaysLeft.removeAt(0)
+            fishDaysLeft[6] = fishDaysLeft[6] + toRespawn
+            fishDaysLeft.add(toRespawn)
+        }
+
+        return fishDaysLeft.sum()
+    }
+
+    private fun parseInitialState(input: String) = input.split(",").map(String::toInt)
 }
--- src/test/kotlin/aoc2021/Puzzle06Tests.kt
+++ src/test/kotlin/aoc2021/Puzzle06Tests.kt
@@ -1,7 +1,8 @@
 package aoc2021
 
 import TestHelpers.readLinesFromResource
-import aoc2021.Puzzle06.countFishAfterDays
+import aoc2021.Puzzle06.naiveCountFishAfterDays
+import aoc2021.Puzzle06.optimisedCountFishAfterDays
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -11,11 +12,23 @@ internal class Puzzle06Tests {
 
     @Test
     fun `80 days example`() {
-        assertEquals(5934, countFishAfterDays(80, exampleState))
+        assertEquals(5934, naiveCountFishAfterDays(80, exampleState))
+        assertEquals(5934, optimisedCountFishAfterDays(80, exampleState))
     }
 
     @Test
     fun `80 days answer`() {
-        assertEquals(358214, countFishAfterDays(80, answerState.first()))
+        assertEquals(358214, naiveCountFishAfterDays(80, answerState.first()))
+        assertEquals(358214, optimisedCountFishAfterDays(80, answerState.first()))
+    }
+
+    @Test
+    fun `256 days example`() {
+        assertEquals(26984457539, optimisedCountFishAfterDays(256, exampleState))
+    }
+
+    @Test
+    fun `256 days answer`() {
+        assertEquals(1622533344325, optimisedCountFishAfterDays(256, answerState.first()))
     }
 }
