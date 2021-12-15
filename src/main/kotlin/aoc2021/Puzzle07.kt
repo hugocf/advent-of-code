2f587660a3babe1ee6587e3f69f199e7bc7b7228
Hugo Ferreira <hugocf@users.noreply.github.com>
2021-12-15 08:37:21 +0000

    2021: Puzzle 7 part 2

--- src/main/kotlin/aoc2021/Puzzle07.kt
+++ src/main/kotlin/aoc2021/Puzzle07.kt
@@ -4,11 +4,22 @@ import aoc2021.Math.median
 import kotlin.math.abs
 
 object Puzzle07 {
-    fun minimumFuelCost(input: String): Int {
+    fun minimumConstantFuelCost(input: String): Int {
         val positions = parseInitialState(input)
         val targetPosition = positions.median().toInt()
 
         return positions.sumOf { abs(it - targetPosition) }
+    }
+
+    fun minimumLinearFuelCost(input: String): Int {
+        val positions = parseInitialState(input)
+        val lowest = positions.minOrNull() ?: 0
+        val highest = positions.maxOrNull() ?: 0
+        val frequency = positions.groupingBy { it }.eachCount()
+        val costBetween = { start: Int, stop: Int -> (0..abs(start - stop)).sum() }
+        val fuelCostTo = { target: Int -> frequency.entries.sumOf { costBetween(it.key, target) * it.value } }
+
+        return (lowest..highest).map { fuelCostTo(it) }.minOf { it }
     }
 
     private fun parseInitialState(input: String) = input.split(",").map(String::toInt)
--- src/test/kotlin/aoc2021/Puzzle07Tests.kt
+++ src/test/kotlin/aoc2021/Puzzle07Tests.kt
@@ -1,7 +1,8 @@
 package aoc2021
 
 import TestHelpers.readLinesFromResource
-import aoc2021.Puzzle07.minimumFuelCost
+import aoc2021.Puzzle07.minimumConstantFuelCost
+import aoc2021.Puzzle07.minimumLinearFuelCost
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -10,12 +11,22 @@ internal class Puzzle07Tests {
     private val answerPositions = readLinesFromResource("2021/puzzle07.txt")
 
     @Test
-    fun `the example`() {
-        assertEquals(37, minimumFuelCost(examplePositions))
+    fun `constant fuel spend example`() {
+        assertEquals(37, minimumConstantFuelCost(examplePositions))
     }
 
     @Test
-    fun `the answer`() {
-        assertEquals(351901, minimumFuelCost(answerPositions.first()))
+    fun `constant fuel spend answer`() {
+        assertEquals(351901, minimumConstantFuelCost(answerPositions.first()))
+    }
+
+    @Test
+    fun `linear fuel spend example`() {
+        assertEquals(168, minimumLinearFuelCost(examplePositions))
+    }
+
+    @Test
+    fun `linear fuel spend answer`() {
+        assertEquals(101079875, minimumLinearFuelCost(answerPositions.first()))
     }
 }
